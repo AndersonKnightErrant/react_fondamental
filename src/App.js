cfba3d504905f76c4bdb857a5b89b9f481576043
AndersonKnightErrant <andrew83anderson@gmail.com>
2022-08-03 23:11:10 +0400

    add api handler url requests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './styles/App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
 
@@ -7,12 +7,19 @@ import PostFilter from './PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from './components/UI/button/MyButton';
 import { usePosts } from './components/hooks/usePosts';
+import PostService from './components/API/PostService';
 
 function App() {
 	const [posts, setPosts] = useState([]);
 	const [filter, setFilter] = useState({ sort: '', query: '' });
 	const [modal, setModal] = useState(false);
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+	
+	useEffect(() => {
+		console.log('useff')
+		fetchPosts()
+	}, [])
+	
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost]);
@@ -20,12 +27,20 @@ function App() {
 	}
 
 	// Получаем пост из дочернего элемента
+	async function fetchPosts() {
+		const posts = await PostService.getAll();
+		setPosts(posts)
+	}
+
 	const removePost = (post) => {
 		setPosts(posts.filter(p => p.id !== post.id))
 	}
 
 	return (
 		<div className='app'>
+			<MyButton onClick={fetchPosts}>
+				Загрузить посты
+			</MyButton>
 			<MyButton style={{ marginTop: '20px'}} onClick={() => setModal(true)}>
 				Coздать пост
 			</MyButton>
