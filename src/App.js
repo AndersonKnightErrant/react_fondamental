ab0a7a9d28e786520e3f21b25246471d5130ccf3
AndersonKnightErrant <andrew83anderson@gmail.com>
2022-08-05 13:46:52 +0400

    add navigation pages of posts

--- src/App.js
+++ src/App.js
@@ -10,64 +10,78 @@ import { usePosts } from './components/hooks/usePosts';
 import PostService from './components/API/PostService';
 import Loader from './components/UI/Loader/Loader';
 import useFetching from './components/hooks/useFetching';
+import { getPageCount, getPagesArray } from './utils/pages';
 
 function App() {
-	const [posts, setPosts] = useState([]);
-	const [filter, setFilter] = useState({ sort: '', query: '' });
-	const [modal, setModal] = useState(false);
-	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-	const [totalCount, setTotalCount] = useState(0);
-	const [limit, setLimit] = useState(10);
-	const [page, setPage] = useState(1);
-	
-	const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
-		const response = await PostService.getAll(limit, page)
-		setPosts(response.data)
-		console.log(response.headers['x-totalCount'])
-		setTotalCount(response.headers['x-totalCount'])
-	})
-	
-	useEffect(() => {
-		fetchPosts()
-	}, [])
-	
+  const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState({ sort: '', query: '' });
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [totalPages, settotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
 
-	const createPost = (newPost) => {
-		setPosts([...posts, newPost]);
-		setModal(false);
+  const pagesArray = getPagesArray(totalPages);
+
+  const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
+    const response = await PostService.getAll(limit, page);
+
+    setPosts(response.data);
+    const totalCount = response.headers['x-total-count'];
+    settotalPages(getPageCount(totalCount, limit));
+  });
+
+  useEffect(() => {
+    fetchPosts();
+  }, [page]);
+
+  console.log(totalPages);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+    setModal(false);
+  };
+
+  // Получаем post из дочернего элемента
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+	};
+	
+	const changePage = (page) => {
+		setPage(page);
 	}
 
-
-	const removePost = (post) => {
-		setPosts(posts.filter(p => p.id !== post.id))
-	}
-
-	return (
-		<div className='app'>
-			<MyButton onClick={fetchPosts}>
-				Загрузить посты
-			</MyButton>
-			<MyButton style={{ marginTop: '20px'}} onClick={() => setModal(true)}>
-				Coздать пост
-			</MyButton>
-			<MyModal visible={modal} setVisible={setModal}>
-				<PostForm create={createPost} />
-			</MyModal>
-			<hr style={{ margin: '15px 0' }} />
-			<PostFilter
-				filter={filter}
-				setFilter={setFilter}
-			/>
-			{postError &&
-				<h1>Произошла ошибка ${postError}</h1>
-			}
-			{isPostLoading
-				? <div style={{display:'flex', 'justifyContent': 'center', 'marginTop': '50px'}}><Loader /></div>
-				: <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Посты'} />
-			}
-				
-		</div>
-	);
+  return (
+    <div className='app'>
+      <MyButton onClick={fetchPosts}>Загрузить посты</MyButton>
+      <MyButton style={{ marginTop: '20px' }} onClick={() => setModal(true)}>
+        Coздать пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+      <hr style={{ margin: '15px 0' }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      {postError && <h1>Произошла ошибка ${postError}</h1>}
+      {isPostLoading ? (
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+          <Loader />
+        </div>
+      ) : (
+        <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Посты'} />
+      )}
+      <div className='page__wrapper'>
+        {pagesArray.map((p) => (
+          <span
+            onClick={() => changePage(p)}
+            key={p}
+            className={page === p ? 'page__current' : 'page'}>
+            {p}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
