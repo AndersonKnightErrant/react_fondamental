ace56cb0e73b146680f5dc54983863d505ac8dad
AndersonKnightErrant <andrew83anderson@gmail.com>
2022-08-05 22:52:02 +0400

    made a template for links inside the router application

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,10 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Navbar from '../components/UI/Navbar/Navbar';
-import About from '../pages/About';
-import Posts from '../pages/Posts';
-import Error from '../pages/Error';
-import PostsIdPag from '../pages/PostIdPage'
+import { routes } from './router';
 
 
 const AppRouter = () => {
@@ -12,11 +9,12 @@ const AppRouter = () => {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/about' element={<About/>} />
-				<Route path='/posts' element={<Posts />} />
-				<Route exact path='/posts/:id' element={<PostsIdPag/>} />
-        <Route exact path='/error' element={<Error/>} />
-        <Route path='*' element={<Error />} />
+				{routes.map(route => 
+					<Route key={route.path}
+						path={route.path}
+						element={<route.element/>}
+					/>
+				)}
       </Routes>
     </Router>
   );
